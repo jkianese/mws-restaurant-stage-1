230dff0fb71c222956744def1522d26afaa1e951
Jim Kianese <jkianese@gmail.com>
2018-09-03 17:00:03 -0400

    Rubric now, pretty later
    Rubric now, pretty later

--- sw.js
+++ sw.js
@@ -1,71 +1,39 @@
-console.log('Service Worker is registered');
+/* console.log('Service Worker is registered'); */
 
-
-
-const cacheFiles = [
-    '/',
-    '/index.html',
-    '/restaurant.html',
-    '/css/styles.css',
-    '/js/dbhelper.js',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    '/data/restaurants.json',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg'
-];
-
-/* MC WT 
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open('v1').then(function(cache) {
-            return cache.addAll(cacheFiles);
-        })
-    );
-});
-*/
-
-/* From MDN Web Docs ExtendableEvent.waitUntil
- *  Install Event in Service Workers
- *
- *   addEventListener('install', event => {
- *       event.waitUntil(async function() {
- *           const cache = await caches.open('static-v1');
- *           await cache.addAll([
- *               '/', '/about/', '/static/styles.css'
- *           ]);
- *       }());
- *   });
- */
-addEventListener('install', event => {
+self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('static-v1').then(function(cache) {
-            return cache.addAll(cacheFiles);
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/restaurant.html',
+                '/css/styles.css',
+                '/js/dbhelper.js',
+                '/js/main.js',
+                '/js/restaurant_info.js',
+                '/data/restaurants.json',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg'
+            ]);
         })
     );
 });
 
-/* MDN Web Docs
- *  FetchEvent.respondWith()
- */
-
-addEventListener('fetch', event => {
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(function(response) {
             if (response) {
-                console.log('Found ', event.request, ' in cache');
                 return response;
             }
             else {
-                console.log('Could not find ', event.request, ' in cache');
                 return fetch(event.request)
                 .then(function(response) {
                     const clonedResponse = response.clone(); 
