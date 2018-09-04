3cf9a6a4c62dd9589f1a1368fb3d6d0f6a8a0fa7
Jim Kianese <jkianese@gmail.com>
2018-09-04 15:27:33 -0400

    Fix SW for offline
    Fix SW for offline

--- sw.js
+++ sw.js
@@ -1,51 +1,55 @@
-/* console.log('Service Worker is registered'); */
+const cacheName = 'static-v1';
 
+const cacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+];
+
+// Call Install Event
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('static-v1').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/restaurant.html',
-                '/css/styles.css',
-                '/js/dbhelper.js',
-                '/js/main.js',
-                '/js/restaurant_info.js',
-                '/data/restaurants.json',
-                '/img/1.jpg',
-                '/img/2.jpg',
-                '/img/3.jpg',
-                '/img/4.jpg',
-                '/img/5.jpg',
-                '/img/6.jpg',
-                '/img/7.jpg',
-                '/img/8.jpg',
-                '/img/9.jpg',
-                '/img/10.jpg'
-            ]);
+        caches
+        .open(cacheName)
+        .then(cache => {
+            cache.addAll(cacheFiles);
         })
     );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            if (response) {
-                return response;
-            }
-            else {
-                return fetch(event.request)
-                .then(function(response) {
-                    const clonedResponse = response.clone(); 
-                    caches.open('static-v1').then(function(cache) {
-                        cache.put(event.request, clonedResponse);
-                    })
-                    return response;
-                })
-                .catch(function(err) {
-                    console.error(err);
-                });
-            }
-        })
+// Call Activate Event
+self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.filter(function(cacheName) {
+          }).map(function(cacheFiles) {
+            return caches.delete(cacheFiles);
+          })
+        );
+      })
     );
-});
+  });
+
+  // Call Fetch Event
+  self.addEventListener('fetch', event => {
+      event.respondWith(
+          fetch(event.request).catch(() => caches.match(event.request)));
+  }); 
+  
+
